cc3614c17686b1492f98996e43678181bd76329c
jguerrab <jguerrab@unal.edu.co>
2024-03-09 08:28:00 -0500

    intento 1

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,26 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import nltk
 import pandas as pd
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",
+                     thousands=None,  # separador de miles para números
+                     decimal=".",  # separador de decimales
+                     )
+    df=df.copy()
+    df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x) #limpiar espacios en blancos
+    df = df.applymap(lambda x: x.lower() if isinstance(x, str) else x) # Convertir todos los datos a minúsculas
+    df=df.drop_duplicates() #elimiar duplicados
+    df=df.dropna()#eliminar filas vacias
+    df.monto_del_credito=df.monto_del_credito.str.strip("$") #eliminar simbologia
+    df.monto_del_credito=df.monto_del_credito.replace({',':''},regex=True).apply(pd.to_numeric) 
+                  # para quitar el separador decimal y, a continuación, utiliza pd.to_numeric() 
+                 #para convertir las columnas a tipo numérico. El resultado se muestra en la consola.
+    df.comuna_ciudadano=df.comuna_ciudadano.astype(int)
     return df
+
+print(clean_data())
